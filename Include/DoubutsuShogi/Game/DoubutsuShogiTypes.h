4ec0e86936b753b1505296194bc0f05936a82e1f
Takahiro Itou <Takahiro Itou@localhost>
2015-07-25 08:37:50 +0900

    ??????? (1) ???????????????

---
+++ Include/DoubutsuShogi/Game/DoubutsuShogiTypes.h
@@ -0,0 +1,33 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---   Doubutsu  Shogi  ---                      **
+**                                                                      **
+**          Copyright (C), 2015-2015, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Type Definitions.
+**
+**      @file   Game/DoubutsuShogiTypes.h
+**/
+
+#if !defined( DSHOGI_GAME_INCLUDED_DOUBUTSU_SHOGI_TYPES_H )
+#    define   DSHOGI_GAME_INCLUDED_DOUBUTSU_SHOGI_TYPES_H
+
+#include    "DoubutsuShogi/.Config/DoubutsuShogiSettings.h"
+
+#include    <stdint.h>
+
+DSHOGI_NAMESPACE_BEGIN
+
+//========================================================================
+//
+//    Type Definitions.
+//
+
+DSHOGI_NAMESPACE_END
+
+#endif
