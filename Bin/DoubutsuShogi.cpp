d5b9af11d2b03b7b278c8d6afe7e56767cca76c2
Takahiro Itou <Takahiro Itou@localhost>
2015-07-25 18:40:49 +0900

    ???????

--- Bin/DoubutsuShogi.cpp
+++ Bin/DoubutsuShogi.cpp
@@ -16,6 +16,7 @@
 
 #include    "DoubutsuShogi/Game/GameController.h"
 
+#include    <iomanip>
 #include    <iostream>
 
 using   namespace   DSHOGI_NAMESPACE;
@@ -26,6 +27,17 @@ int  main(int argc, char * argv[])
     GameController  gc;
 
     gc.resetGameBoard();
+    gc.writeToStream(std::cout) << std::endl;
+
+    //  テスト。ヒヨコを取ってみる。    //
+    const   GameController::BoardState  bs  = gc.getCurrentBoard();
+    const   GameController::ActionData
+        act = GameController::encodeMoveAction(bs, POS_ENC_B3, POS_ENC_B2);
+    std::cout   << "指し手：B3,B2 = 0x"
+                << std::hex << std::setw(16) << std::setfill('0')
+                << act << std::endl;
+
+    gc.playForward(act);
     gc.writeToStream(std::cout) << std::endl;
 
     return ( 0 );
