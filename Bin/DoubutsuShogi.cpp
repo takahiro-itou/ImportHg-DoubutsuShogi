04f962eedf4a630228fa1565c18d28cce7181d9f
Takahiro Itou <Takahiro Itou@localhost>
2015-07-25 10:45:42 +0900

    ???????? (1) ???????????? (2) Makefile.am ???

---
+++ Bin/DoubutsuShogi.cpp
@@ -0,0 +1,26 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---   Doubutsu  Shogi  ---                      **
+**                                                                      **
+**          Copyright (C), 2015-2015, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      アプリケーション本体。
+**
+**      @file   DoubutsuShogi.cpp
+**/
+
+#include    "DoubutsuShogi/Game/GameController.h"
+
+#include    <iostream>
+
+using   namespace   DSHOGI_NAMESPACE;
+
+int  main(int argc, char * argv[])
+{
+    return ( 0 );
+}
