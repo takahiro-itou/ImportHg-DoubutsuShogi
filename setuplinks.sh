9c0a2f827a58c53c7572590f921e594ab9ced8fd
Takahiro Itou <Takahiro Itou@localhost>
2015-07-26 19:42:55 +0900

    ???????????? (1) ???????????????????? (2) ????????

--- setuplinks.sh
+++ setuplinks.sh
@@ -1,10 +1,11 @@
 #! /bin/bash -x
 
+LN_H='ln -f '
 LN_S='ln -sf '
 RM_F='rm -f '
 
 for  dir  in  Game  ; do
    (cd  Lib/${dir}/Tests  \
-      &&  ${LN_S}  ../../Tests/.TestCommon/Makefile.am)
+      &&  ${LN_S}  ../../Tests/.TestsCommon/Makefile.am)
 done
 
