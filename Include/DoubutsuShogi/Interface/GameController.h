3385b0836c56a3f6e6da74e65e8128428ec780f0
Takahiro Itou <Takahiro Itou@localhost>
2015-08-08 18:15:16 +0900

    ???????? (1) ??????????????

---
+++ Include/DoubutsuShogi/Interface/GameController.h
@@ -0,0 +1,111 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---   Doubutsu  Shogi  ---                      **
+**                                                                      **
+**          Copyright (C), 2015-2015, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of GameController class.
+**
+**      @file       Interface/GameController.h
+**/
+
+#if !defined( DSHOGI_INTERFACE_INCLUDED_GAME_CONTROLLER_H )
+#    define   DSHOGI_INTERFACE_INCLUDED_GAME_CONTROLLER_H
+
+#include    "DoubutsuShogi/Common/DoubutsuShogiTypes.h"
+
+DSHOGI_NAMESPACE_BEGIN
+namespace  INTERFACE  {
+
+//========================================================================
+//
+//    GameController  class.
+//
+/**
+**    ゲームの進行管理するクラス。
+**/
+
+class  GameController
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （コンストラクタ）。
+    **
+    **/
+    GameController();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~GameController();
+
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+
+public:
+    //  テストクラス。  //
+    friend  class   GameControllerTest;
+};
+
+}   //  End of namespace  INTERFACE
+DSHOGI_NAMESPACE_END
+
+#endif
---
+++ Lib/Interface/GameController.cpp
@@ -0,0 +1,88 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---   Doubutsu  Shogi  ---                      **
+**                                                                      **
+**          Copyright (C), 2015-2015, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of GameController class.
+**
+**      @file       Interface/GameController.cpp
+**/
+
+#include    "DoubutsuShogi/Interface/GameController.h"
+
+#include    <ostream>
+
+DSHOGI_NAMESPACE_BEGIN
+namespace  INTERFACE  {
+
+//========================================================================
+//
+//    GameController  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （コンストラクタ）。
+//
+
+GameController::GameController()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+GameController::~GameController()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  INTERFACE
+DSHOGI_NAMESPACE_END
