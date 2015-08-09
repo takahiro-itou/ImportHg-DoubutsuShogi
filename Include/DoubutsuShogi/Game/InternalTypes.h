14aff8ff1ac47a46ec3df16563d317975b013dfe
Takahiro Itou <Takahiro Itou@localhost>
2015-08-09 20:54:56 +0900

    ???? (1) ???????????????????????

--- Include/DoubutsuShogi/Game/InternalTypes.h
+++ Include/DoubutsuShogi/Game/InternalTypes.h
@@ -34,24 +34,24 @@ namespace  GAME  {
 enum  FieldConst
 {
     FIELD_EMPTY_SQUARE  = 0x00,     /**<  空いているマス。  **/
-    FIELD_BLACK_PIECE   = 0x10,     /**<  先手の駒。        **/
-    FIELD_WHITE_PIECE   = 0x20,     /**<  後手の駒。        **/
-    FIELD_WALL_SQUARE   = 0x30,     /**<  壁マス（番兵）。  **/
+    FIELD_BLACK_PIECE   = 0x08,     /**<  先手の駒。        **/
+    FIELD_WHITE_PIECE   = 0x10,     /**<  後手の駒。        **/
+    FIELD_WALL_SQUARE   = 0x18,     /**<  壁マス（番兵）。  **/
 
-    FIELD_BLACK_PAWN    = 0x10,     /**<  先手のヒヨコ。    **/
-    FIELD_BLACK_BISHOP  = 0x11,     /**<  先手のゾウ。      **/
-    FIELD_BLACK_ROOK    = 0x12,     /**<  先手のキリン。    **/
-    FIELD_BLACK_KING    = 0x13,     /**<  先手のライオン。  **/
-    FIELD_BLACK_GOLD    = 0x14,     /**<  先手のニワトリ。  **/
+    FIELD_BLACK_PAWN    = 0x08,     /**<  先手のヒヨコ。    **/
+    FIELD_BLACK_BISHOP  = 0x09,     /**<  先手のゾウ。      **/
+    FIELD_BLACK_ROOK    = 0x0A,     /**<  先手のキリン。    **/
+    FIELD_BLACK_KING    = 0x0B,     /**<  先手のライオン。  **/
+    FIELD_BLACK_GOLD    = 0x0C,     /**<  先手のニワトリ。  **/
 
-    FIELD_WHITE_PAWN    = 0x20,     /**<  後手のヒヨコ。    **/
-    FIELD_WHITE_BISHOP  = 0x21,     /**<  後手のゾウ。      **/
-    FIELD_WHITE_ROOK    = 0x22,     /**<  後手のキリン。    **/
-    FIELD_WHITE_KING    = 0x23,     /**<  後手のライオン。  **/
-    FIELD_WHITE_GOLD    = 0x24,     /**<  後手のニワトリ。  **/
+    FIELD_WHITE_PAWN    = 0x10,     /**<  後手のヒヨコ。    **/
+    FIELD_WHITE_BISHOP  = 0x11,     /**<  後手のゾウ。      **/
+    FIELD_WHITE_ROOK    = 0x12,     /**<  後手のキリン。    **/
+    FIELD_WHITE_KING    = 0x13,     /**<  後手のライオン。  **/
+    FIELD_WHITE_GOLD    = 0x14,     /**<  後手のニワトリ。  **/
 
     /**   盤上にどちらの駒があるかを取得するためのマスク。  **/
-    FIELD_MASK_PIECE_OWNER  = 0x30
+    FIELD_MASK_PIECE_OWNER  = 0x18
 };
 
 //----------------------------------------------------------------
--- Lib/Game/BoardState.cpp
+++ Lib/Game/BoardState.cpp
@@ -19,6 +19,41 @@
 DSHOGI_NAMESPACE_BEGIN
 namespace  GAME  {
 
+namespace  {
+
+//----------------------------------------------------------------
+/**
+**    画面表示の処理時に参照する座標の変換表。
+**/
+
+//----------------------------------------------------------------
+/**
+**    画面表示の処理時に参照する駒番号の変換表。
+**/
+
+constexpr   INTERFACE::PieceIndex
+s_tblPieceConv[FIELD_WALL_SQUARE + 1]   =
+{
+    INTERFACE::PIECE_EMPTY,         INTERFACE::NUM_PIECE_TYPES,
+    INTERFACE::NUM_PIECE_TYPES,     INTERFACE::NUM_PIECE_TYPES,
+    INTERFACE::NUM_PIECE_TYPES,     INTERFACE::NUM_PIECE_TYPES,
+    INTERFACE::NUM_PIECE_TYPES,     INTERFACE::NUM_PIECE_TYPES,
+
+    INTERFACE::PIECE_BLACK_PAWN,    INTERFACE::PIECE_BLACK_BISHOP,
+    INTERFACE::PIECE_BLACK_ROOK,    INTERFACE::PIECE_BLACK_KING,
+    INTERFACE::PIECE_BLACK_GOLD,    INTERFACE::NUM_PIECE_TYPES,
+    INTERFACE::NUM_PIECE_TYPES,     INTERFACE::NUM_PIECE_TYPES,
+
+    INTERFACE::PIECE_WHITE_PAWN,    INTERFACE::PIECE_WHITE_BISHOP,
+    INTERFACE::PIECE_WHITE_ROOK,    INTERFACE::PIECE_WHITE_KING,
+    INTERFACE::PIECE_WHITE_GOLD,    INTERFACE::NUM_PIECE_TYPES,
+    INTERFACE::NUM_PIECE_TYPES,     INTERFACE::NUM_PIECE_TYPES,
+
+    INTERFACE::NUM_PIECE_TYPES
+};
+
+}   //  End of (Unnamed) namespace.
+
 //========================================================================
 //
 //    BoardState  class.
@@ -59,6 +94,26 @@ BoardState::~BoardState()
 ErrCode
 BoardState::resetGameBoard()
 {
+    for ( int y = 0; y < GPOS_NUM_ROWS; ++ y ) {
+        for ( int x = 0; x < GPOS_NUM_COLS; ++ x ) {
+            this->m_bsField[y][x]   = FIELD_EMPTY_SQUARE;
+        }
+    }
+
+    this->m_bsField[GPOS_ROW_1][GPOS_COL_A] = FIELD_WHITE_ROOK;
+    this->m_bsField[GPOS_ROW_1][GPOS_COL_B] = FIELD_WHITE_KING;
+    this->m_bsField[GPOS_ROW_1][GPOS_COL_C] = FIELD_WHITE_BISHOP;
+    this->m_bsField[GPOS_ROW_2][GPOS_COL_B] = FIELD_WHITE_PAWN;
+
+    this->m_bsField[GPOS_ROW_3][GPOS_COL_B] = FIELD_BLACK_PAWN;
+    this->m_bsField[GPOS_ROW_4][GPOS_COL_A] = FIELD_BLACK_BISHOP;
+    this->m_bsField[GPOS_ROW_4][GPOS_COL_B] = FIELD_BLACK_KING;
+    this->m_bsField[GPOS_ROW_4][GPOS_COL_C] = FIELD_BLACK_ROOK;
+
+    for ( int hp = 0; hp < FIELD_WALL_SQUARE; ++ hp ) {
+        this->m_nHands[hp]  = 0;
+    }
+
     return ( ERR_FAILURE );
 }
 
@@ -75,6 +130,30 @@ ErrCode
 BoardState::copyToViewBuffer(
         INTERFACE::ViewBuffer  &bufView)  const
 {
+    using   namespace   INTERFACE;
+
+    for ( int y = 0; y < POS_NUM_ROWS; ++ y ) {
+        for ( int x = 0; x < POS_NUM_COLS; ++ x ) {
+            const   FieldConst  fc  = this->m_bsField[y + 1][x + 1];
+            bufView.piBoard[y * POS_NUM_COLS + x]   = s_tblPieceConv[fc];
+        }
+    }
+
+    for ( int hp = INTERFACE::PIECE_BLACK_PAWN;
+            hp < INTERFACE::PIECE_BLACK_GOLD; ++ hp )
+    {
+        const  int  hs  = hp + FIELD_BLACK_PAWN - INTERFACE::PIECE_BLACK_PAWN;
+        bufView.nHands[hp]  = this->m_nHands[hs];
+    }
+
+    for ( int hp = INTERFACE::PIECE_WHITE_PAWN;
+            hp < INTERFACE::PIECE_WHITE_GOLD; ++ hp )
+    {
+        const  int  hs  = hp + FIELD_WHITE_PAWN - INTERFACE::PIECE_WHITE_PAWN;
+        bufView.nHands[hp]  = this->m_nHands[hs];
+    }
+
+
     return ( ERR_FAILURE );
 }
 
