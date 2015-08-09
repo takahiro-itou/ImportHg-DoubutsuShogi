c6c7f133cd7e0188cababd82659dfa4fa44fc292
Takahiro Itou <Takahiro Itou@localhost>
2015-08-09 18:23:02 +0900

    ??????? (1) ???????????????

---
+++ Include/DoubutsuShogi/Game/InternalTypes.h
@@ -0,0 +1,33 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---   Doubutsu  Shogi  ---                      **
+**                                                                      **
+**          Copyright (C), 2015-2015, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of Internal Types.
+**
+**      @file       Game/InternalTypes.h
+**/
+
+#if !defined( DSHOGI_GAME_INCLUDED_INTERNAL_TYPES_H )
+#    define   DSHOGI_GAME_INCLUDED_INTERNAL_TYPES_H
+
+#include    "DoubutsuShogi/Common/DoubutsuShogiTypes.h"
+
+DSHOGI_NAMESPACE_BEGIN
+namespace  GAME  {
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+}   //  End of namespace  GAME
+DSHOGI_NAMESPACE_END
+
+#endif
