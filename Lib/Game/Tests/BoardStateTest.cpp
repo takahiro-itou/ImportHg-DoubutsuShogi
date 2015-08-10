ae44274ad5e7b26b33a925da506203f2336013bb
Takahiro Itou <Takahiro Itou@localhost>
2015-08-10 11:38:54 +0900

    ????????

--- Lib/Game/Tests/BoardStateTest.cpp
+++ Lib/Game/Tests/BoardStateTest.cpp
@@ -14,6 +14,8 @@
 **      @file       Game/Tests/BoardStateTest.cpp
 **/
 
+#include    "Tests/TestDriver.h"
+
 #include    "DoubutsuShogi/Game/BoardState.h"
 
 #include    <iomanip>
@@ -22,11 +24,28 @@
 DSHOGI_NAMESPACE_BEGIN
 namespace  GAME  {
 
+//========================================================================
+//
+//    BoardStateTest  class.
+//
+/**
+**    単体テストクラス。
+**/
+
+class  BoardStateTest : public  CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE(BoardStateTest);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    virtual  void   setUp()     override    { }
+    virtual  void   tearDown()  override    { }
+};
 
 }   //  End of namespace  GAME
 DSHOGI_NAMESPACE_END
 
 int  main(int argc, char * argv[])
 {
-    return ( 0 );
+    return ( executeCppUnitTests(argc, argv) );
 }
