4248befd8b245e067ce395915edfa1a5657fe2d1
Takahiro Itou <Takahiro Itou@localhost>
2015-08-14 01:31:46 +0900

    ??????????

--- Lib/Game/Tests/BoardStateTest.cpp
+++ Lib/Game/Tests/BoardStateTest.cpp
@@ -32,11 +32,27 @@ namespace  GAME  {
 class  BoardStateTest : public  CPPUNIT_NS::TestFixture
 {
     CPPUNIT_TEST_SUITE(BoardStateTest);
+    CPPUNIT_TEST(testDecodeActionData);
+    CPPUNIT_TEST(testIsLegalAction);
+    CPPUNIT_TEST(testPlayBackward);
+    CPPUNIT_TEST(testPlayForward);
+    CPPUNIT_TEST(testResetGameBoard);
+    CPPUNIT_TEST(testCopyToViewBuffer);
+    CPPUNIT_TEST(testCurrentState);
     CPPUNIT_TEST_SUITE_END();
 
 public:
     virtual  void   setUp()     override    { }
     virtual  void   tearDown()  override    { }
+
+private:
+    void  testDecodeActionData();
+    void  testIsLegalAction();
+    void  testPlayBackward();
+    void  testPlayForward();
+    void  testResetGameBoard();
+    void  testCopyToViewBuffer();
+    void  testCurrentState();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION( BoardStateTest );
@@ -45,6 +61,34 @@ CPPUNIT_TEST_SUITE_REGISTRATION( BoardStateTest );
 //
 //    Tests.
 //
+
+void  BoardStateTest::testDecodeActionData()
+{
+}
+
+void  BoardStateTest::testIsLegalAction()
+{
+}
+
+void  BoardStateTest::testPlayBackward()
+{
+}
+
+void  BoardStateTest::testPlayForward()
+{
+}
+
+void  BoardStateTest::testResetGameBoard()
+{
+}
+
+void  BoardStateTest::testCopyToViewBuffer()
+{
+}
+
+void  BoardStateTest::testCurrentState()
+{
+}
 
 }   //  End of namespace  GAME
 DSHOGI_NAMESPACE_END
