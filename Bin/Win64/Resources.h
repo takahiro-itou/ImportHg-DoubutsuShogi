b9e342cd40d9a3d45125ab228eda2e1b1186a331
Takahiro Itou <Takahiro Itou@localhost>
2015-08-22 17:29:27 +0900

    ?????????

--- Bin/Win64/Resources.h
+++ Bin/Win64/Resources.h
@@ -13,3 +13,22 @@
 **
 **      @file       Bin/Win64/Resources.h
 **/
+
+#if !defined( DSHOGI_BIN_WIN64_INCLUDED_RESOURCES_H )
+#    define   DSHOGI_BIN_WIN64_INCLUDED_RESOURCES_H
+
+//========================================================================
+//
+//    メニューリソース。
+//
+
+enum  MenuResource
+{
+    MENU_ID_FILE        = 10000,
+    MENU_ID_FILE_EXIT   = 10001,
+    MENU_ID_HELP        = 10100,
+    MENU_ID_HELP_SHOW   = 10101,
+};
+
+#endif
+
