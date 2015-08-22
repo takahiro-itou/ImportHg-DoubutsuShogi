02a8c9597222df923697ccf8cc4d9f90e6480efb
Takahiro Itou <Takahiro Itou@localhost>
2015-08-22 13:43:16 +0900

    ???????????????

---
+++ Bin/Win64/DoubutsuShogiWin64.cpp
@@ -0,0 +1,27 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---   Doubutsu  Shogi  ---                      **
+**                                                                      **
+**          Copyright (C), 2015-2015, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      アプリケーション本体。
+**
+**      @file       Bin/Win64/DoubutsuShogiWin64.cpp
+**/
+
+#include    <windows.h>
+
+int  APIENTRY
+WinMain(
+        HINSTANCE   hInst,
+        HINSTANCE,
+        LPSTR       lpCmdLine,
+        int         nCmdShow)
+{
+    return ( 0 );
+}
